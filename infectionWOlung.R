aa59b007e4dbcebd4505c9b01cf4559ceb503209
rayanh <rtg5200@psu.edu>
2025-04-28 14:07:58 -0400

    putting in infected FFE and fixed some ODE stuff

--- infectionWOlung.R
+++ infectionWOlung.R
@@ -40,7 +40,7 @@ ui <- fluidPage(
                ),
                
                mainPanel( 
-                 width = 8,
+                 width = 6,
                  fluidRow(
                    column(12, plotOutput("plot1"))   # Plot 1 in its own row
                  ),
@@ -48,7 +48,10 @@ ui <- fluidPage(
                    column(12, plotOutput("plot2"))   # Plot 2 in its own row
                  ),
                  fluidRow(
-                   column(12, plotOutput("plot3"))   # Plot 3 in its own row
+                   column(12, plotOutput("plot3"))   # Plot 3 in its own row 
+                 ), 
+                 fluidRow(
+                   column(12, plotOutput("plot4"))   # Plot 4 in its own row
                  )
                )
              )
@@ -64,28 +67,33 @@ sir_equations <- function(time, variables, parameters) {
     dB <- -M * B_cells - beta * Cb * B_cells - beta_2 * Ct * B_cells + (g1 * (Cb + Ct) / (g2 + (Cb + Ct))) - mu_o*B_cells + mu_p*B_cells
     dCb <- M * B_cells + beta * Cb * B_cells + beta_2 * Ct * B_cells - mu_o*Cb + mu_p*Cbb_cells - alpha*Cb
     dT <- -M * T_cells - nu_A * Cb * T_cells - nu_b * Ct * T_cells + (h1 * (Cb + Ct) / (h2 + (Cb + Ct))) - mu_o*T_cells + mu_p*Tb_cells 
-    dAt <- M * T_cells + nu_A * Cb * T_cells + nu_b * Ct * T_cells - beta_2 * Ct * At - beta * Cb * At -M*At - mu_o*At + mu_p*Atb_cells
-    dLt <- theta * (beta_2 * Ct * At + beta * Cb * At) - mu_o*Lt + mu_p*Ltb_cells #GET RID OF LATENT T CELLS AT SOME RATE 
-    dCt <- (1 - theta) * (beta_2 * Ct * At + beta * Cb * At) - alpha_2 * Ct + M*At - mu_o*Ct + mu_p*Ctb_cells
-    
+    dAt <- M * T_cells + nu_A * Cb * T_cells + nu_b * Ct * T_cells - beta_2 * Ct * At - beta * Cb * At - 2*M*At - mu_o*At + mu_p*Atb_cells
+    dLt <- theta * (beta_2 * Ct * At + beta * Cb * At) - mu_o*Lt + mu_p*Ltb_cells + M*At - gamma*Lt 
+    dCt <- (1 - theta) * (beta_2 * Ct * At + beta * Cb * At) - alpha_2 * Ct + M*At - mu_o*Ct + mu_p*Ctb_cells + M*At 
+    dZ <- gamma*Lt 
     
     ### entering blood ###   
     dBb <- mu_o*B_cells + mu_o*B_Th - 2 * mu_p*Bb_cells - alpha_B*Bb_cells   
     dCbb <- mu_o*Cb + mu_o*Cb_Th - 2 * mu_p*Cbb_cells - alpha_B*Cbb_cells   # removed mu_o B cells b/c lung dynamics not being modelled 
     dTb <- mu_o*T_cells + mu_o*T_Th - 2 * mu_p*Tb_cells - alpha_B*Tb_cells  
     dAtb <- mu_o*At + mu_o*At_Th  - 2 * mu_p*Atb_cells # no death for At cells 
-    dLtb <- mu_o*theta*(beta_2 * Ct * At + beta * Cb * At) + mu_o * Lt - 2 * mu_p*Ltb_cells # population of Lt?  
-    dCtb <- mu_o*Ct - 2*mu_p*Ctb_cells    
+    dLtb <- mu_o* Lt_Th + mu_o * Lt - 2 * mu_p*Ltb_cells  # population of Lt?  
+    dCtb <- mu_o*Ct + mu_o*Ct_Th - 2*mu_p*Ctb_cells    
     
     ### Thymus ### 
     dB_Th <- -M * B_Th  - beta * Cb_Th* B_Th - beta_2 * Ct_Th * B_Th + (g1 * (Cb_Th + Ct_Th) / (g2 + (Cb_Th + Ct_Th))) - mu_o*B_Th + mu_p*Bb_cells 
     dCb_Th <- M * B_Th + beta * Cb_Th * B_Th + beta_2 * Ct_Th * B_Th - alpha * Cb_Th - mu_o*Cb_Th + mu_p*Cbb_cells 
     dT_Th <- - M * T_Th -nu_A * Cb_Th * T_Th - nu_b * Ct_Th * T_Th + (h1 * (Cb_Th + Ct_Th) / (h2 + (Cb_Th + Ct_Th))) - mu_o*T_Th + mu_p*Tb_cells 
-    dAt_Th <- M * T_Th + nu_A * Cb_Th * T_Th + nu_b * Ct_Th * T_Th - beta_2 * Ct_Th * At_Th - beta*Cb_Th*At_Th - M*At_Th - mu_o*At_Th + mu_p * Atb_cells 
-    dLt_Th <- theta * (beta_2 * Ct_Th * At_Th + beta * Cb_Th * At_Th) - mu_o*Lt_Th + mu_p*Ltb_cells 
-    dCt_Th <- (1 - theta) * (beta_2 * Ct_Th * At_Th + beta * Cb_Th * At_Th) - alpha_2 * Ct_Th + M*At_Th - mu_o*Ct_Th + mu_p*Ctb_cells    
+    dAt_Th <- M * T_Th + nu_A * Cb_Th * T_Th + nu_b * Ct_Th * T_Th - beta_2 * Ct_Th * At_Th - beta*Cb_Th*At_Th - 2*M*At_Th - mu_o*At_Th + mu_p * Atb_cells 
+    dLt_Th <- theta * (beta_2 * Ct_Th * At_Th + beta * Cb_Th * At_Th) - mu_o*Lt_Th + mu_p*Ltb_cells + M*At_Th - gamma*Lt_Th 
+    dCt_Th <- (1 - theta) * (beta_2 * Ct_Th * At_Th + beta * Cb_Th * At_Th) - alpha_2 * Ct_Th + M*At_Th - mu_o*Ct_Th + mu_p*Ctb_cells     
+    dZ_Th <- gamma * Lt_Th 
     
-    return(list(c(dB, dCb, dT, dAt, dLt, dCt, dBb, dCbb, dTb, dAtb, dLtb, dCtb, dB_Th, dCb_Th, dT_Th, dAt_Th, dLt_Th, dCt_Th)))
+    ### FFE ###  
+    dF <- -Ltb_cells*f*epsilon
+    dIF <- Ltb_cells*f*epsilon
+    
+    return(list(c(dB, dCb, dT, dAt, dLt, dCt, dZ, dBb, dCbb, dTb, dAtb, dLtb, dCtb, dB_Th, dCb_Th, dT_Th, dAt_Th, dLt_Th, dCt_Th, dZ_Th, dF, dIF)))
   })
 }
 
@@ -106,23 +114,27 @@ server <- function(input, output) {
       , nu_b = input$nu_B                #Activation rate with B cells (166 hours/ 41days)
       , alpha = input$alpha                #death rate of B cells (every 33 hours)
       , alpha_2 = input$alpha_2        #death rate of T cells (every 48 hours) 
-      , alpha_B = input$alpha_B
+      , alpha_B = input$alpha_B 
+      , gamma = 1/30                 #rate of tumor formation 
       , theta = 0.7                 #population of activated T cells 
       , g1 = input$g1          #incoming B cells (every 15 hours)
       , g2 = input$g2    
       , h1 = input$h1                      #incoming T cells / determined no incoming T cells 
-      , h2 = input$h2
+      , h2 = input$h2 
+      , epsilon = 1/30                      #feather follicle infection rate 
     )
   })
   
   # Define initial values for the model
-  initial_values <- reactive({c( 
+  initial_values <- reactive({c(  
+    ### bursa ### 
     B_cells = 1000,  
     Cb = 0, 
     T_cells =10,
     At = 0,
     Lt = 0,
-    Ct = 0, 
+    Ct = 0,  
+    Z = 0, 
     ### blood ### 
     Bb_cells = 100, 
     Cbb_cells = 0, 
@@ -136,7 +148,11 @@ server <- function(input, output) {
     T_Th = 1000,
     At_Th = 0,
     Lt_Th = 0,
-    Ct_Th = 0
+    Ct_Th = 0, 
+    Z_Th = 0, 
+    ### FFE ### 
+    f = 20, 
+    If = 0
   )}) 
   # Solve the ODE model using the parameters
   sir_values_1 <- reactive({
@@ -161,9 +177,10 @@ server <- function(input, output) {
       lines(time, Cb, col = "green") 
       lines(time, At, col = "blue") 
       lines(time, Lt, col= "purple") 
-      lines(time, Ct, col = "yellow")
-      legend("topright", legend = c("B_cells", "T_cells", "Cb", "At", "Lt", "Ct"),
-             col = c("black", "red", "green", "blue", "purple", "yellow"), lty = 1, bty = "n")
+      lines(time, Ct, col = "yellow") 
+      lines(time, Z, col = "orange") 
+      legend("topright", legend = c("B_cells", "T_cells", "Cb", "At", "Lt", "Ct", "Z"),
+             col = c("black", "red", "green", "blue", "purple", "yellow", "orange"), lty = 1, bty = "n")
     })
   })
   
@@ -179,7 +196,7 @@ server <- function(input, output) {
       lines(time, Tb_cells, col = "green") 
       lines(time, Atb_cells, col = "blue") 
       lines(time, Ltb_cells, col = "purple") 
-      lines(time, Ctb_cells, col = "yellow")
+      lines(time, Ctb_cells, col = "yellow") 
       legend("topright", legend = c("Bb", "Cbb", "Tb", "Atb", "Ltb", "Ctb"),
              col = c("black", "red", "green", "blue", "purple", "yellow"), lty = 1, bty = "n")
     })
@@ -196,9 +213,22 @@ server <- function(input, output) {
       lines(time, Cb_Th, col = "green") 
       lines(time, At_Th, col = "blue") 
       lines(time, Lt_Th, col= "purple") 
-      lines(time, Ct_Th, col = "yellow")
-      legend("topright", legend = c("B_cells", "T_cells", "Cb", "At", "Lt", "Ct"),
-             col = c("black", "red", "green", "blue", "purple", "yellow"), lty = 1, bty = "n")
+      lines(time, Ct_Th, col = "yellow") 
+      lines(time, Z_Th, col = "orange") 
+      legend("topright", legend = c("B_cells", "T_cells", "Cb", "At", "Lt", "Ct", "Z"),
+             col = c("black", "red", "green", "blue", "purple", "yellow", "orange"), lty = 1, bty = "n")
+    })
+  })
+    output$plot4 <- renderPlot({
+    sol <- sir_values_1()
+    sol_df <- as.data.frame(sol)
+    
+    with(sol_df, {
+      plot(time, f, col = "black", type = "l", ylim = c(0, 1000), xlab = "Time (days)",
+           ylab = "Population density", main = "Feather Follicles", cex.lab = 1.5, xlim = c(0, 500/24))
+      lines(time, If, col = "red")
+      legend("topright", legend = c("Feather Follicles", "Infected Feather Follicles"),
+             col = c("black", "red"), lty = 1, bty = "n")
     })
   })
   
